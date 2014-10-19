9c7c5165df4d744905abc57c235fa0466b260971
Gerald Bauer <gerald.bauer@gmail.com>
2014-10-19 20:23:03 +0200

    up repo

--- settings.rb
+++ settings.rb
@@ -3,15 +3,15 @@
 
 # -- output / repos  --
 
-US_ROOT    = './o/us-united-states'
+# US_ROOT    = './o/us-united-states'
 # CA_ROOT    = './o/ca-canada'
-BE_ROOT    = './o/be-belgium'
-DE_ROOT    = './o/de-deutschland'
-WORLD_ROOT = './o/world'
+# BE_ROOT    = './o/be-belgium'
+# DE_ROOT    = './o/de-deutschland'
+# WORLD_ROOT = './o/world'
 
-## US_ROOT    = '../us-united-states'
+US_ROOT    = '../us-united-states'
 CA_ROOT    = '../ca-canada'
-## BE_ROOT    = '../be-belgium'
-## DE_ROOT    = '../de-deutschland'
-## WORLD_ROOT = '../world'
+BE_ROOT    = '../be-belgium'
+DE_ROOT    = '../de-deutschland'
+WORLD_ROOT = '../world'
 
